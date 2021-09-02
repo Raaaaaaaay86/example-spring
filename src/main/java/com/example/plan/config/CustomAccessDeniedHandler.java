57a737e098c7cd765f5951794810eb4349d5fdc6
TW - IT - Ray Lin <ray.lin@shoalter.com>
2021-09-02 18:10:57 +0800

    feat_客製403 httpResponse

---
+++ src/main/java/com/example/plan/config/CustomAccessDeniedHandler.java
@@ -0,0 +1,26 @@
+package com.example.plan.config;
+
+import org.json.JSONObject;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+  @Override
+  public void handle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AccessDeniedException e) throws IOException, ServletException {
+    var responseJSON = new JSONObject();
+    responseJSON.put("timestamp", new Date().getTime());
+    responseJSON.put("status", "403");
+    responseJSON.put("message", "權限不足");
+    httpServletResponse.setHeader("content-type", "application/json");
+    httpServletResponse.setCharacterEncoding("UTF-8");
+    httpServletResponse.getWriter().println(responseJSON.toString());
+  }
+}
--- src/main/java/com/example/plan/config/WebSecurityConfig.java
+++ src/main/java/com/example/plan/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.plan.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -8,11 +9,15 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.AccessDeniedHandler;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Autowired
+  private AccessDeniedHandler customDeniedHandler;
 
   @Override
   public void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -36,6 +41,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
       .anyRequest().authenticated()
       .and()
       .httpBasic()
+      .and()
+      .exceptionHandling().accessDeniedHandler(customDeniedHandler)
       .and()
       .csrf().disable();
   }
