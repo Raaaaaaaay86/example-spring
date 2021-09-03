0653e6dad497de8d52d956b9f0f2f8185a3e25ba
TW - IT - Ray Lin <ray.lin@shoalter.com>
2021-09-03 13:51:43 +0800

    feat_ CustomJsonSerialzer for redistemplate and cacheAnnotation

--- src/main/java/com/example/plan/config/RedisConfig.java
+++ src/main/java/com/example/plan/config/RedisConfig.java
@@ -1,23 +1,27 @@
 package com.example.plan.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.*;
 
 import java.lang.reflect.Method;
 
 @Configuration
 @EnableCaching
 public class RedisConfig extends CachingConfigurerSupport {
+
+  @Autowired
+  private RedisConnectionFactory redisConnectionFactory;
 
   @Bean
   public KeyGenerator keyGenerator() {
@@ -35,21 +39,33 @@ public class RedisConfig extends CachingConfigurerSupport {
     };
   }
 
-  @SuppressWarnings("rawtypes")
+//  @SuppressWarnings("rawtypes")
   @Bean
-  public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
-    return RedisCacheManager.create(connectionFactory);
+  public CacheManager cacheManager(RedisTemplate redisTemplate) {
+    RedisCacheWriter redisCacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(redisTemplate.getConnectionFactory());
+    RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+      .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(redisTemplate.getValueSerializer()));
+
+    return new RedisCacheManager(redisCacheWriter, redisCacheConfiguration);
   }
 
   @Bean
-  public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
-    StringRedisTemplate template = new StringRedisTemplate(factory);
-    Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
-    ObjectMapper objectMapper = new ObjectMapper();
-    jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
-    template.setValueSerializer(jackson2JsonRedisSerializer);
-    template.afterPropertiesSet();
-    return template;
+  public RedisTemplate<String, Object> redisTemplate() {
+    RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+    redisTemplate.setConnectionFactory(this.redisConnectionFactory);
+
+    Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<Object>(Object.class);
+    StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+
+    redisTemplate.setKeySerializer(stringRedisSerializer);
+    redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+
+    redisTemplate.setHashKeySerializer(stringRedisSerializer);
+    redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+
+    redisTemplate.setEnableDefaultSerializer(true);
+    redisTemplate.setDefaultSerializer(jackson2JsonRedisSerializer);
+    return redisTemplate;
   }
 
 }
--- src/main/java/com/example/plan/controller/LogController.java
+++ src/main/java/com/example/plan/controller/LogController.java
@@ -1,25 +1,43 @@
 package com.example.plan.controller;
 
+import com.example.plan.entity.Student;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 @RequestMapping("/log")
 public class LogController {
 
-  private Logger logger = Logger.getLogger(LogController.class);
+  private Logger logger = Logger.getLogger("stdout1");
+
+  @Autowired
+  private RedisTemplate<String, Object> redisTemplate;
 
   @GetMapping("/")
-  public String printHello(ModelMap model) {
-    logger.debug("DEBUG LEVEL");
-    logger.info("INFO LEVEL");
-    logger.warn("WARN LEVEL");
-    logger.error("ERROR LEVEL");
-    logger.fatal("FATAL LEVEL");
-    return "Hello";
+  public Object printHello(ModelMap model) {
+    logger.debug("Hello /log");
+    var student = new Student();
+    student.setName("王王王");
+    student.setId(99);
+    student.setSex("male");
+    student.setBirthday(876182400000L);
+
+    redisTemplate.opsForValue().set("student::99", student);
+    return "re";
+  }
+
+  @GetMapping("/aa")
+  public Object test() {
+    var re = redisTemplate.opsForValue().get("student::54");
+    logger.debug(re);
+    return "ss";
   }
 }
