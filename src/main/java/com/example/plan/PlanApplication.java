eba7d78d0f04bb5a1423eb4449847f17d75abd00
TW - IT - Ray Lin <ray.lin@shoalter.com>
2021-09-06 14:54:04 +0800

    feat_ switchable .properties file

--- src/main/java/com/example/plan/PlanApplication.java
+++ src/main/java/com/example/plan/PlanApplication.java
@@ -2,13 +2,10 @@ package com.example.plan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-@PropertySources({@PropertySource("log4j.properties"), @PropertySource("redis.properties"), @PropertySource("rabbitmq.properties")})
 public class PlanApplication {
 
 	public static void main (String[] args) {
